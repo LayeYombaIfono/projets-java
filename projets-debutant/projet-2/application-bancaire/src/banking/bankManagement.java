5a640b7728107225784268cb9555f297d9bdd050
Laye Yomba IFONO <layeyomba12@gmail.com>
2025-01-10 11:49:42 +0000

    Fonction de création de compte ok

--- projets-debutant/projet-2/application-bancaire/src/banking/bankManagement.java
+++ projets-debutant/projet-2/application-bancaire/src/banking/bankManagement.java
@@ -1,4 +1,51 @@
 package banking;
 
+import java.sql.Connection;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+
 public class bankManagement {
+
+    private static final int NULL = 0;
+
+    static Connection con = connection.getConnection();
+    static String sql = "";
+
+    /**
+     * Fonction pour créer un compte
+     * @param name nom du client
+     * @param passCode code de compte
+     * @return Vrai ou fausse
+     */
+    public static boolean createAccount(String name, int passCode){
+
+        try {
+
+//            Validation des informations
+            if (name.isEmpty() || passCode == NULL){
+                System.out.println("Tous les champs sont obligatoires !");
+                return false;
+            }
+            // Requête pour inserer des informations
+            Statement stm = con.createStatement();
+            sql = "INSERT INTO customer(cname, balance, pass_code) VALUES('" + name + "', 1000" + passCode + ")";
+
+            // Exécuté la requête
+            if (stm.executeUpdate(sql)==1){
+                System.out.println(name + " Connectez-vous maintenant");
+                return true;
+            }
+            // Retourne
+        }
+        catch (SQLIntegrityConstraintViolationException e){
+            System.out.println("Nom d'utilisateur non disponible");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    //Retourne
+        return false;
+    }
+
 }
